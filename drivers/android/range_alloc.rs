634cd8f5f082cde77797382a745ea1eb8d3dee56
mattgilbride <mattgilbride@google.com>
2023-03-07 21:25:12 +0000

    rust_binder: range_alloc: kunit tests

--- drivers/android/range_alloc.rs
+++ drivers/android/range_alloc.rs
@@ -3,6 +3,7 @@
 use core::ptr::NonNull;
 use kernel::{
     linked_list::{CursorMut, GetLinks, Links, List},
+    macros::kunit_tests,
     prelude::*,
 };
 
@@ -185,5 +186,146 @@ impl<T> GetLinks for Descriptor<T> {
     type EntryType = Self;
     fn get_links(desc: &Self) -> &Links<Self> {
         &desc.links
+    }
+}
+
+#[kunit_tests(rust_android_binder_driver_range_alloc)]
+mod tests {
+    use crate::range_alloc::RangeAllocator;
+    use crate::range_alloc::Descriptor;
+    use crate::range_alloc::DescriptorState;
+    use kernel::linked_list::Cursor;
+    use kernel::prelude::*;
+    #[test]
+    fn test_reserve_new() {
+        let mut ra: RangeAllocator<usize> = RangeAllocator::new(10).unwrap();
+        let mut expected = [Some(DescriptorState::Free), None, None, None];
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+        let offset = ra.reserve_new(4).unwrap();
+        expected = [Some(DescriptorState::Reserved), Some(DescriptorState::Free), None, None];
+        assert_eq!(offset, 0);
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+        let offset = ra.reserve_new(5).unwrap();
+        expected = [Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Free), None];
+        assert_eq!(offset, 4);
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+        let offset = ra.reserve_new(1).unwrap();
+        expected = [Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), None];
+        assert_eq!(offset, 9);
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+        let offset = ra.reserve_new(2);
+        assert!(offset.is_err());
+    }
+
+    #[test]
+    fn test_reservation_abort_no_merge() {
+        let mut ra: RangeAllocator<usize> = RangeAllocator::new(10).unwrap();
+        let mut expected = [Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Free)];
+        ra.reserve_new(2).unwrap();
+        let offset_middle = ra.reserve_new(2).unwrap();
+        ra.reserve_new(2).unwrap();
+
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+
+        ra.reservation_abort(offset_middle).unwrap();
+        expected = [Some(DescriptorState::Reserved), Some(DescriptorState::Free), Some(DescriptorState::Reserved), Some(DescriptorState::Free)];
+
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+    }
+
+    #[test]
+    fn test_reservation_abort_merge_right() {
+        let mut ra: RangeAllocator<usize> = RangeAllocator::new(10).unwrap();
+        let mut expected = [Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Free)];
+        ra.reserve_new(2).unwrap();
+        ra.reserve_new(2).unwrap();
+        let offset_right = ra.reserve_new(2).unwrap();
+
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+
+        ra.reservation_abort(offset_right).unwrap();
+        expected = [Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Free), None];
+
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+    }
+
+    #[test]
+    fn test_reservation_abort_merge_left() {
+        let mut ra: RangeAllocator<usize> = RangeAllocator::new(10).unwrap();
+        let mut expected = [Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Free)];
+        let offset_left = ra.reserve_new(2).unwrap();
+        let offset_middle = ra.reserve_new(2).unwrap();
+        ra.reserve_new(2).unwrap();
+
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+
+        ra.reservation_abort(offset_left).unwrap();
+        expected = [Some(DescriptorState::Free), Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Free)];
+
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+
+        ra.reservation_abort(offset_middle).unwrap();
+        expected = [Some(DescriptorState::Free), Some(DescriptorState::Reserved), Some(DescriptorState::Free), None];
+
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+    }
+
+    #[test]
+    fn test_reservation_abort_merge_both() {
+        let mut ra: RangeAllocator<usize> = RangeAllocator::new(10).unwrap();
+        let mut expected = [Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Reserved), Some(DescriptorState::Free)];
+        let offset_left = ra.reserve_new(2).unwrap();
+        let offset_middle = ra.reserve_new(2).unwrap();
+        let offset_right = ra.reserve_new(2).unwrap();
+
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+
+        ra.reservation_abort(offset_left).unwrap();
+        ra.reservation_abort(offset_right).unwrap();
+        expected = [Some(DescriptorState::Free), Some(DescriptorState::Reserved), Some(DescriptorState::Free), None];
+
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+
+        ra.reservation_abort(offset_middle).unwrap();
+        expected = [Some(DescriptorState::Free), None, None, None];
+
+        assert!(check_invariant_and_state(ra.list.cursor_front(), expected).is_none());
+    }
+
+    fn check_invariant_and_state(cursor: Cursor<Box<Descriptor<usize>>>, expected: [Option<DescriptorState>; 4]) -> Option<&str> {
+        let mut cursor = cursor;
+        let mut last = None;
+        let mut index = 0;
+        while let Some(desc) = cursor.current() {
+            if let Some((last_offset, last_size, last_state)) = last {
+                if desc.offset != last_offset + last_size {
+                    return Some("offset/size invariant violated!");
+                }
+                match (last_state, &desc.state) {
+                    (&DescriptorState::Free, &DescriptorState::Free) => {
+                        return Some("adjacent free descriptors invariant violated!")
+                    },
+                    _ => {}
+                }
+            }
+            match expected[index] {
+                None => return Some("more descriptors than expected!"),
+                Some(ref expected_state) => {
+                    if expected_state != &desc.state {
+                        return Some("unexpected descriptor state!")
+                    }
+                }
+            }
+            last = Some((&desc.offset, &desc.size, &desc.state));
+            index += 1;
+            cursor.move_next();
+        }
+        while index < 4 {
+            if expected[index].is_some() {
+                return Some("more expected than descriptors!")
+            }
+            index += 1
+        }
+        None
     }
 }

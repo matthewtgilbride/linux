f72b4311bf637426a228f55a67144447b78e56c7
Wedson Almeida Filho <walmeida@microsoft.com>
2023-07-03 15:42:44 +0200

    rust: file: add Rust abstraction for `struct file`
    Using this abstraction it becomes possible to access files from drivers
    written in Rust. Add support for accessing the flags, and for managing
    the refcount of the file.
    
    Change-Id: I60eb42e15f5ec50652c70b7061ab980b6505a47f
    Signed-off-by: Wedson Almeida Filho <walmeida@microsoft.com>
    Co-developed-by: Daniel Xu <dxu@dxuuu.xyz>
    Signed-off-by: Daniel Xu <dxu@dxuuu.xyz>
    Co-developed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/helpers.c
+++ rust/helpers.c
@@ -156,6 +156,12 @@ void rust_helper_init_work_with_key(struct work_struct *work, work_func_t func,
 	work->func = func;
 }
 EXPORT_SYMBOL_GPL(rust_helper_init_work_with_key);
+ 
+struct file *rust_helper_get_file(struct file *f)
+{
+	return get_file(f);
+}
+EXPORT_SYMBOL_GPL(rust_helper_get_file);
 
 /*
  * `bindgen` binds the C `size_t` type as the Rust `usize` type, so we can
